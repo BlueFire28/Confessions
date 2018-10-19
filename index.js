a833d5b727b80b227b2a236f921c98a5145e4d14
steven4547466 <23726534+steven4547466@users.noreply.github.com>
2018-10-19 14:00:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,6 @@ bot.on('message', async message => {
         if (!message.member.roles.has(pending.id)) return message.channel.send(sender + ", you are not in-progress!")
         if(userData[sender.id].appsNumber === 5) return message.channel.send(sender + ', you have exceeded your maximum number of applications, if this is a mistake, please contact <@186487324517859328> or <@353782817777385472>')
         userData[sender.id].appsNumber = (userData[sender.id].appsNumber+1)
-        console.log(userData[sender.id].appsNumber)
         let m = await message.reply('I have notified the staff that you have applied, please ensure that your answer\'s are at least a paragraph long, if they are not, your application will be discarded.')
         
         let m1 = await appchannel.send(`<@&${Staff.id}>`)
