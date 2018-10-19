9ab669cf0c4d6ef8db77a040d99a4693f00c72ea
Rinkky <vurvur1@hotmail.gr>
2018-10-19 15:12:16 +0300

    apps update

--- index.js
+++ index.js
@@ -87,16 +87,42 @@ bot.on('message', async message => {
     if (msg === prefix + 'applied'){
         let appchannel = message.guild.channels.find(`name`, "staff")
         let pending = message.guild.roles.find('name', "In-Progress")    
-        if (!message.member.roles.has(pending.id)) return message.channel.send(sender + ", you are not in progress!")
-        let m = await message.reply('I have notified the staff that you have applied, please ensure that you\'re answers are at least a paragraph long, if they are not, your application will be discarded.')
+        if (!message.member.roles.has(pending.id)) return message.channel.send(sender + ", you are not in-progress!")
+
+        if (!appsNumber[sender.id]) appsNumber[sender.id] = {apps: 0};
+                    let AppsData = appsNumber[sender.id];
+
+                    fs.writeFile("./storage/userData.json", JSON.stringify(appsNumber), (err) => {
+                      if (err) console.error(err)
+                    });
+
+        if(AppsData.apps === 5) return message.channel.send(sender + ', you have exceeded your maximum number of applications, if this is a mistake, please contact <@186487324517859328> or <@353782817777385472>')
+        AppsData.apps++
+        let m = await message.reply('I have notified the staff that you have applied, please ensure that your answer\'s are at least a paragraph long, if they are not, your application will be discarded.')
         
         let m1 = await appchannel.send(`<@&${Staff.id}>`)
         let applyEmbed = new Discord.RichEmbed()
         .setDescription("**___New application___**")
         .setColor(0x15f153)
         .addField('Name:', sender)
+        .addField("ID", sender.id)
+        .addField("applied At", message.createdAt)
 
         appchannel.send(applyEmbed)
+        .then(message.guild.members.get("186487324517859328")
+        .createDM()
+        .then(dm => {
+          dm.send({embed: {
+            color: 0xff0000,
+            title: "application DM" ,
+           description: `BlockCraft Has a new application you need to review, please do immidiately.` ,
+           timestamp: new Date(),
+            footer: {
+            icon_url: "353782817777385472".avatarURL,
+            text: "New Application"
+            }
+          }}).catch(error)
+        }))
     };
  
     // Deny
@@ -105,14 +131,14 @@ bot.on('message', async message => {
       let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
       let rreason = args.join(" ").slice(22)
       message.delete()
-      if (!message.member.roles.has(Owner.id) && !message.member.roles.has(Staff.id)) return message.channel.send("You do not have access to this command")
-      if (!rUser) return message.channel.send('This user doesn\'t exist')
+      if (!message.member.roles.has(Owner.id) && !message.member.roles.has(Staff.id)) return message.channel.send("Your role cant handle that much power!")
+      if (!rUser) return message.channel.send('This user is non existent')
       let denyEmbed = new Discord.RichEmbed()
       .setDescription("**___User Denied___**")
       .setColor(0xFF0000)
       .addField('Name of user denied:', rUser)
       .addField('Reason', rreason)
-      .addField('Retry', "You are good to retry as long as you haven't been denied multiple times. Just apply again!")
+      .addField('Retry', "Dont worry, You are allowed to apply again!")
       message.guild.channels.find(`name`, "pending").send(denyEmbed)
     };
     
@@ -122,15 +148,15 @@ bot.on('message', async message => {
       let args = msg.split(" ").slice(1)
       let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
       message.delete()
-      if (!message.member.roles.has(Owner.id) && !message.member.roles.has(Staff.id)) return message.channel.send("You do not have access to this command")
-      if (!rUser) return message.channel.send('This user doesn\'t exist')
+      if (!message.member.roles.has(Owner.id) && !message.member.roles.has(Staff.id)) return message.channel.send("Your role cant handle that much power!")
+      if (!rUser) return message.channel.send('This user is non existent')
       rUser.addRole(PlayerRole.id);
       rUser.removeRole(pending.id);
-      message.guild.channels.find(`name`, "general").send(`Welcome our newest member, ${rUser}!`)
+      message.guild.channels.find(`name`, "general").send(`Welcome our newest member, ${rUser}! \n\n You will be whitelisted soon! Make sure to check the #info, #faq and #announcements channels, for any updates! Claim your land in #claimed-land channel, throw your ideas on #ideas and have a good one!`)
     };
 
     // Delete msgs
-    if (msg.split(" ")[0] === prefix + "mdelete"){
+    if (msg.split(" ")[0] === prefix + "purge"){
         if(sender.id === "186487324517859328" || message.member.roles.has(Owner.id)) {
             let args = msg.split(" ").slice(1)
             let num = Number(args[0]);
