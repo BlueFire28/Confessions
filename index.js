79c2051002c3948ea78083a3ae10e298ba17d327
steven4547466 <23726534+steven4547466@users.noreply.github.com>
2018-10-20 22:47:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -575,7 +575,8 @@ bot.on('message', async message => {
         if(!message.member.voiceChannel) return message.channel.send("You aren't in a voice channel!")
         if(!serverQueue) return message.channel.send("Nothing is playing!")
 	serverQueue.voiceChannel.leave();
-	serverQueue.songs = []
+	serverQueue.songs = [1];
+	serverQueue.connection.dispatcher.end();
         return undefined;
     }else if(msg === prefix + "skip"){
         if(!message.member.voiceChannel) return message.channel.send("You aren't in a voice channel!")
