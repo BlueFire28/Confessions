0466b8df7063d60b70889b47510f8e1834edc5f3
steven4547466 <23726534+steven4547466@users.noreply.github.com>
2018-11-01 13:18:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ bot.on('message', async message => {
     if(msg === "hey alexa"){
     	await message.channel.send("Boop.")
 	    try{
-		var response = await message.channel.awaitMessages(message2 => message2.sender === message.sender, {
+		var response = await message.channel.awaitMessages(message2 => message2.sender.id === sender.id, {
 				maxMatches: 1,
 				time: 60000,
 				errors: ['time']
