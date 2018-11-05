f3c57e3750ebb01f1b15475864ed2a747f416ab7
steven4547466 <23726534+steven4547466@users.noreply.github.com>
2018-11-05 11:38:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ bot.on('message', async message => {
 	    }catch(err){
 		return await message.channel.send('Beep.')
 	    }
-	    const command = response.content.toLowerCase();
+	    const command = response.first().content.toLowerCase();
 	    if(command === "hi"){
 	    	return await message.channel.send(`Hi, ${message.author}`)
 	    }
